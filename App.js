72d9912046431730bcb2ef83216174c5225290a0
Carol Forsythe <89415254+CAROLGFORSYTHE@users.noreply.github.com>
2021-12-14 10:59:50 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+// 1d
+import Bonus from "./bonus/Bonus";
+
+// 3f
+import Exercise from "./exercise/Exercise";
+
+function App() {
+  return (
+    <div>
+      {/* 1d */}
+      <Exercise />
+      {/* 3g */}
+      <Bonus />
+    </div>
+  );
+}
+
+export default App;
